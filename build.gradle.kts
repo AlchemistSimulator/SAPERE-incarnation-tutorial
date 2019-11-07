ffef4b2b173c74b83397c6a7871da2e5c8ecc182
Danilo Pianini <danilo.pianini@unibo.it>
2019-11-07 18:57:41 +0100

    Auto-detect runnable tasks

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,21 @@ dependencies {
 	implementation("it.unibo.alchemist:alchemist:+")
 }
 
+File(rootProject.rootDir.path + "/src/main/yaml").listFiles()
+    .filter { it.name.matches(Regex("""\d{2}-.*\.yml""")) }
+    .forEach {
+        tasks.register<JavaExec>(it.nameWithoutExtension) {
+            group = "Run Alchemist"
+            description = "Launches simulation ${it.nameWithoutExtension}"
+            main = "it.unibo.alchemist.Alchemist"
+            classpath = sourceSets["main"].runtimeClasspath
+            args(
+                "-y", it.absolutePath,
+                "-g", "effects/${it.nameWithoutExtension}.aes"
+            )
+        }
+    }
+
 // task "runAlchemist"(type: JavaExec) {
 // 	if (project.hasProperty("simulation")) {
 // 		classpath = sourceSets.main.runtimeClasspath
